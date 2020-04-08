3a23a11c42fa11557d8ccbfeea089adfcf31133a
Cedric Duriau <duriau.cedric@live.be>
2020-04-08 23:50:02 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+# stdlib modules
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+# tool modules
+f = os.path.abspath(__file__)
+package_dir = os.path.join(os.path.dirname(f), "python")
+sys.path.insert(0, package_dir)
+from workoutbuddy import __version__  # noqa
+
+requirements_install = ["SQLAlchemy==1.3.*"]
+requirements_dev = ["flake8==3.7.*", "radon==4.1.*"]
+
+
+setup(name="workoutbuddy",
+      version=__version__,
+      description="Workout schedule manager/plotter.",
+      license="MIT",
+      author="C&eacute;dric Duriau",
+      author_email="duriau.cedric@live.be",
+      url="https://github.com/cedricduriau/workoutbuddy",
+      packages=find_packages(where="python"),
+      package_dir={"": "python"},
+      scripts=["bin/workoutbuddy"],
+      install_requires=requirements_install,
+      extras_require={"dev": requirements_dev},
+      data_files=[(os.path.expanduser("~/.workoutbuddy"), [])])
