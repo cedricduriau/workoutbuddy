72f1a45ff599b4dd2609e7ab1ec454e8dcd5aa25
Cedric Duriau <duriau.cedric@live.be>
2020-04-11 14:38:31 +0200

    add docstrings

--- python/workoutbuddy/workoutbuddy.py
+++ python/workoutbuddy/workoutbuddy.py
@@ -22,9 +22,9 @@ DB_PATH = os.path.join(CACHE_DIR, "workoutbuddy.db")
 # =============================================================================
 def _create_engine():
     """
-    Creates an SQLAlchemy database engine.
+    Create an SQLAlchemy database engine.
 
-    :rtype: Engine
+    :rtype: sqlalchemy.engine.Engine
     """
     engine_url = f"sqlite:///{DB_PATH}"
     engine = create_engine(engine_url)
@@ -32,10 +32,26 @@ def _create_engine():
 
 
 def _record_to_dict(r):
+    """
+    Build a dictionary from a database record.
+
+    :param r: Datbase record.
+    :type r: sqlalchemy.engine.result.ResultMetaData
+
+    :rtype: dict
+    """
     return dict(zip(r.keys(), r))
 
 
 def _get_records(table):
+    """
+    Return all records of a database table.
+
+    :param table: SQLAlchemy table model.
+    :type table: Base
+
+    :rtype: list[sqlalchemy.engine.result.ResultMetaData]
+    """
     session = create_session()
     query = select("*").select_from(table)
     result = session.execute(query).fetchall()
@@ -43,6 +59,12 @@ def _get_records(table):
 
 
 def _list_records(table):
+    """
+    Print all records of a database table.
+
+    :param table: SQLAlchemy table model.
+    :type table: Base
+    """
     records = _get_records(table)
     dicts = list(map(_record_to_dict, records))
     for d in dicts:
@@ -53,6 +75,11 @@ def _list_records(table):
 # public
 # =============================================================================
 def create_session():
+    """
+    Create a database session.
+
+    :rtype: sqlalchemy.orm.session.Session
+    """
     engine = _create_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -60,25 +87,37 @@ def create_session():
 
 
 def set_up():
+    """Set up the database and tables."""
     engine = _create_engine()
     Base.metadata.create_all(engine)
 
 
 def tear_down():
+    """Drop all database records."""
     engine = _create_engine()
     Base.metadata.drop_all(engine)
     set_up()
 
 
 def list_exercises():
+    """List all exercise database records."""
     _list_records(Exercise)
 
 
 def list_logs():
+    """List all exercise log records."""
     _list_records(Log)
 
 
 def create_exercise(name):
+    """
+    Create an exercise.
+
+    :param name: Name of the exercise.
+    :type name: str
+
+    :rtype: Exercise
+    """
     session = create_session()
 
     exercise = session.query(Exercise).filter(Exercise.name == name).first()
@@ -93,6 +132,21 @@ def create_exercise(name):
 
 
 def log_exercise(date, exerciseid, reps):
+    """
+    Log an exercise.
+
+    :param date: Date of the performed exercise.
+                 Format DD/MM/YYYY. Use 'today' for current date.
+    :type date: str
+
+    :param exerciseid: ID of the exercise to log.
+    :type exerciseid: int
+
+    :param reps: Amount of repetitions.
+    :type reps: int
+
+    :rtype: Log
+    """
     if date == "today":
         date = datetime.date.today()
     else:
